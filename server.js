c6cee3343f8a60255190964215413ee661a3c030
inboxsgk <30730201+inboxsgk@users.noreply.github.com>
2017-08-27 07:16:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var config = {
     user: 'inboxsgk'
     database: 'inboxsgk'
     host: 'db.imad.hasura-app.io'
-    port: '5432'
+   
     password: process.env.DB_PASSWORD
-}
+};
 
 var app = express();
 app.use(morgan('combined'));
